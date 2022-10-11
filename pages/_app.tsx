e2e86a90ca495dc153d69a56c704abf75cdbb56c
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-10-11 12:44:58 -0500

    Added context and state to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import '../styles/globals.css';
+import { AppContext } from 'context';
+import { useAppState } from 'hooks/useAppState';
+import { ProtectedRoute } from 'components/protectedRoute';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const state = useAppState();
+
+  return (
+    <AppContext.Provider value={state}>
+      <ProtectedRoute>
+        <Component {...pageProps} />
+      </ProtectedRoute>
+    </AppContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
